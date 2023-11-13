cd8f9631ac60f237d431d7c057ebe1336e055200
Kaylea Dutt <kaydutt1138@gmail.com>
2023-11-13 14:59:54 -0700

    Final commit

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,30 +1,31 @@
 function randomNumber(num) {
-    return Math.floor(Math.random()* num)
+    return Math.floor(Math.random() *num)
 }
 
 const sayings = {
-    array1: ['do it!', 'do hard things!', 'do great things!'],
-    array2: ['be great!', 'be the day!', 'be productive!'],
-    array3: ['holds great things!', 'is up to you!', 'will be great!']
+    array1: ['Will be great', 'will be awesome', 'will be ok'],
+    array2: ['do hard things', 'make good choices', 'achieve anything'],
+    array3: ['be amazing', 'do anything', 'be positive']
 }
 
-let finalSaying = []
+const finalSaying = []
 
 for (let i in sayings) {
-    let randomSaying = randomNumber(sayings[i].length)
+    const randomSaying = randomNumber(sayings[i].length)
 
     switch(i) {
         case 'array1':
-            finalSaying.push(`You can ${sayings[i][randomSaying]}`)
-            break;
-        case 'array2':
             finalSaying.push(`Today will ${sayings[i][randomSaying]}`)
-            break;
+        break;
+        case 'array2':
+            finalSaying.push(`You can ${sayings[i][randomSaying]}`)
+        break;
         case 'array3':
-            finalSaying.push(`The future ${sayings[i][randomSaying]}`)
-            break;
-            default:
-            finalSaying.push('There was an error sadly!');
+            finalSaying.push(`You will ${sayings[i][randomSaying]}`)
+        break;
+        default:
+            finalSaying.push('Not enough info sorry!')
+        break;
     }
 }
 
